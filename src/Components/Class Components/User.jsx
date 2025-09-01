976b2889378a1bb8e84790e5c313213d59b1bc0f
harshkale612 <harshkale612@gmail.com>
2025-09-01 15:41:00 +0530

    Update User.jsx

--- src/Components/Class Components/User.jsx
+++ src/Components/Class Components/User.jsx
@@ -8,7 +8,7 @@ const User = ({ name }) => {
       <h1>Name : {name}</h1>
       <h2>Count = {count1}</h2>
       <h2>Count = {count2}</h2>
-      <h2>Location : Dehradun</h2>
+      <h2>Location : Pandharpur</h2>
       <h3>contact : @akshaymarch7</h3>
     </div>
   );
